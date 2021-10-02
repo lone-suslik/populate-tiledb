53f12adc6b89bc831fb13cb84e2adab9a759a3d1
suslik <dunordavind@protonmail.com>
2021-10-02 12:27:32 +0200

    Implemented schema and imlemented ingestion of a non-random sample

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from elasticsearch import Elasticsearch
-
+#!/usr/bin/env python
 
 # This script populates elasticstore with the sample data.
-# /study accession/contrast accession/formula/gene/pvalue/fdr/fold change/other usual deseq2 columns per gene/
-# The procedure is the following:
-# 1. Generate study id as GSF\d\d\d\d
-# 2. Generate contrast accession as GSC\d\d\d\d_n, where \d\d\d\d is the same as in GFS\d\d\d\d of the parent study
-# 3. Generate formula (as a random formula from the list of the 10 different variables)
-# 4. Generate p-value and FC
+# The schema is defined in the generate_study_index function
+# The random study generation is defined in the generate_random_study function
+import random
 
-
-def generate_random_study_object():
-    return
+from elasticsearch import Elasticsearch
 
 
 def generate_study_index(es):
@@ -35,16 +29,75 @@ def generate_study_index(es):
         },
         "mappings": {
             "properties": {
-                "gene": {"type": "keyword"},
-                "sample": {"type": "keyword"},
-                "value": {"type": "integer"}
+                "study": {"type": "keyword"},
+                "contrast": {
+                    "properties": {
+                        "gene": {
+                            "properties": {
+                                "id": {"type": "keyword"},
+                                "pvalue": {"type": "float"},
+                                "fdr": {"type": "float"},
+                                "logFC": {"type": "float"}
+                            }
+                        },
+                        "name": {
+                            "type": "keyword"
+                        },
+                        "formula": {
+                            "type": "text"
+                        }
+                    }
+                }
             }
         }
     }
 
     # fully delete the old index and create a new one
     es.indices.delete(index=index_id, ignore=[400, 404])
-    es.indices.create(index=index_id, body=body, ignore=400)
+    res = es.indices.create(index=index_id, body=body, ignore=400)
+
+    print(res)
+
+
+def generate_random_study():
+    """
+    The procedure is the following:
+    1. Generate study id as GSF\d\d\d\d
+    2. Generate contrast accession as GSC\d\d\d\d_n, where \d\d\d\d is the same as in GFS\d\d\d\d of the parent study
+    3. Generate formula (as a random formula from the list of the 10 different variables)
+    4. Generate p-value and FC
+
+    Returns
+    -------
+
+    """
+
+    study_id = "GSF" + str(random.randrange(1000, 9999))
+
+    # get a list of genes from the example file
+    doc = {
+        "study": study_id,
+        "contrast": {
+            "gene": [
+                {
+                    "id": "a",
+                    "pvalue": 0.01,
+                    "fdr": 0.01,
+                    "logFC": 5
+                },
+                {
+                    "id": "b",
+                    "pvalue": 0.04,
+                    "fdr": 0.7,
+                    "logFC": 3.2
+                },
+            ],
+            "name": "first contrast",
+            "formula": "~ A + B + C"
+        }
+    }
+
+    return doc
 
 
 if __name__ == "__main__":
@@ -53,4 +106,13 @@ if __name__ == "__main__":
     index_id = "studies"
     print("Populating pstore:")
     print("Generating index...")
+
+    # create index
     generate_study_index(es)
+
+    # generate random study
+    doc = generate_random_study()
+
+    # add study to index
+    res = es.index(index=index_id, body=doc)
+    print(res)
