08d01e75a063a9a05aefbfafc84d2e7b8aee5115
suslik <dunordavind@protonmail.com>
2021-10-03 16:50:34 +0200

    Single-index elastisearch schema works well, but querying is annoying.

--- main.py
+++ main.py
@@ -6,11 +6,36 @@
 import random
 
 from elasticsearch import Elasticsearch
+from elasticsearch import helpers
 
 
 def generate_study_index(es, index_id):
     """
-    Create a new ES index and populate it with random data
+    Create a new ES index using the following mappings:
+
+        "mappings": {
+            "properties": {
+                "study": {"type": "keyword"},
+                "contrast": {
+                    "properties": {
+                        "name": {
+                            "type": "keyword"
+                        },
+                        "formula": {
+                            "type": "text"
+                        }
+                    }
+                },
+                "gene": {
+                    "properties": {
+                        "id": {"type": "keyword"},
+                        "pvalue": {"type": "float"},
+                        "fdr": {"type": "float"},
+                        "logFC": {"type": "float"}
+                    }
+                },
+            }
+        }
 
     Parameters
     ----------
@@ -23,24 +48,20 @@ def generate_study_index(es, index_id):
     """
 
     # define mapping schema
+    # max_rescore_window index settings is 200000
     body = {
         "settings": {
             "number_of_shards": 1,
-            "number_of_replicas": 0
+            "number_of_replicas": 0,
+            "max_rescore_window": 200000,
+            "max_result_window": 200000,
         },
         "mappings": {
             "properties": {
                 "study": {"type": "keyword"},
                 "contrast": {
+                    "type": "nested",
                     "properties": {
-                        "gene": {
-                            "properties": {
-                                "id": {"type": "keyword"},
-                                "pvalue": {"type": "float"},
-                                "fdr": {"type": "float"},
-                                "logFC": {"type": "float"}
-                            }
-                        },
                         "name": {
                             "type": "keyword"
                         },
@@ -48,10 +69,20 @@ def generate_study_index(es, index_id):
                             "type": "text"
                         }
                     }
-                }
+                },
+                "gene": {
+                    "properties": {
+                        "id": {"type": "keyword"},
+                        "pvalue": {"type": "float"},
+                        "fdr": {"type": "float"},
+                        "logFC": {"type": "float"}
+                    }
+                },
             }
         }
     }
+
+
 
     # fully delete the old index and create a new one
     es.indices.delete(index=index_id, ignore=[400, 404])
@@ -62,11 +93,10 @@ def generate_study_index(es, index_id):
 
 def generate_random_study(gene_ids, n_contrasts=5, max_fc=100):
     """
-    The procedure is the following:
-    1. Generate study id as GSF\d\d\d\d
-    2. Generate contrast accession as GSC\d\d\d\d_n, where \d\d\d\d is the same as in GFS\d\d\d\d of the parent study
-    3. Generate formula (as a random formula from the list of the 10 different variables)
-    4. Generate p-value and FC
+    Generate a list of random json documents, one document per gene id.
+    Documents correspond to mappings from generate_study_index function.
+    Study id is generated as GSF\d\d\d\d
+    Contrast is generated  GSC\d\d\d\d_n, where \d\d\d\d is the same as in GFS\d\d\d\d of the parent study
     Parameters
     ----------
     max_fc: int, default=100
@@ -74,40 +104,71 @@ def generate_random_study(gene_ids, n_contrasts=5, max_fc=100):
     gene_ids: list of str
     Returns
     -------
-
+    list:
+        list of jsons, one json per gene id
     """
 
     study_tag = str(random.randrange(1000, 9999))
     study_id = "GSF" + study_tag
     contrast_ids = ["GSFC" + study_tag + str(x) for x in range(0, n_contrasts)]
 
-    contrasts = []
+    docs = []
 
     for contrast in contrast_ids:
-        genes = []
-
-        for gene in gene_ids:
-            genes.append({
-                "id": gene,
-                "pvalue": random.random(),
-                "fdr": random.random(),
-                "logFC": random.random() * max_fc
-            })
 
         contrast = {
             "name": contrast,
             "formula": "~ A + B + C",
-            "gene": genes
         }
 
-        contrasts.append(contrast)
+        for gene_id in gene_ids:
+            gene = {
+                "id": gene_id,
+                "pvalue": random.random(),
+                "fdr": random.random(),
+                "logFC": random.random() * max_fc
+            }
 
-    doc = {
-        "study": study_id,
-        "contrast": contrasts
-    }
+            docs.append({
+                "study": study_id,
+                "contrast": contrast,
+                "gene": gene
+            })
 
-    return doc
+    return docs
+
+
+def insert_random_study(es, index_id, docs):
+    """
+    Insert a list of documents into an index indicated by index_id.
+
+
+    Parameters
+    ----------
+    es: Elasticsearch
+        Elasticsearch object
+    index_id: str
+              Name of the index to which to insert the docs,
+    docs:
+
+    Returns
+    -------
+    json:
+        Elasticsearch response json object
+    """
+
+    # add study to index
+    # this is the way to invoke the bulk api as described at:
+    # https://elasticsearch-py.readthedocs.io/en/master/helpers.html#example
+    def iterate_docs():
+        for d in docs:
+            yield {
+                "_index": index_id,
+                "_type": "_doc",
+                "_source": d
+            }
+
+    return helpers.bulk(es, iterate_docs())
 
 
 def main():
@@ -131,12 +192,11 @@ def main():
     # create index
     generate_study_index(es, index_id)
 
-    # generate random study
-    doc = generate_random_study(gene_ids)
-
-    # add study to index
-    res = es.index(index=index_id, body=doc)
-    print(res)
+    # generate 100 random stud
+    for i in range(0, 10):
+        print(f"Inserting study {i}")
+        s = generate_random_study(gene_ids)
+        insert_random_study(es, index_id, s)
 
 
 if __name__ == "__main__":
